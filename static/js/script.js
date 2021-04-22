e925a9fb6809bdb0ab8477f952ada454b64d8b9c
jacksheehy15 <sheehyboi@gmail.com>
2021-04-22 15:26:40 +0000

    Added collabsible accordian to items page

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
+    // Initiliaize Drag from right on touchscreen
     $('.sidenav').sidenav({edge: "right", draggable: "True"});
+    // Initialize Collapsable Component
+     $('.collapsible').collapsible();
 });
