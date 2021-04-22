85c26b458d57fbf02229e8722853d4dcc43e6ea7
jacksheehy15 <sheehyboi@gmail.com>
2021-04-22 16:20:10 +0000

    Added add_item template form

--- app.py
+++ app.py
@@ -97,6 +97,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_item")
+def add_item():
+    return render_template("add_item.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
