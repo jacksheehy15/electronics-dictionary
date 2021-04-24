e20146f2bd8d0eadad16ec0f76583eb70fd68a8f
jacksheehy15 <sheehyboi@gmail.com>
2021-04-24 12:56:55 +0000

    wired up Edit button for items

--- app.py
+++ app.py
@@ -113,6 +113,12 @@ def add_item():
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_item.html", categories=categories)
 
+@app.route("/edit_item/<item_id>", methods=["GET", "POST"])
+def edit_item(item_id):
+    item = mongo.db.item.find_one({"_id": ObjectId(item_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("edit_item.html", item=item, categories=categories)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
