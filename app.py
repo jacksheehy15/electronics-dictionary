cab54fcdf4891c6f910300d374385583ecc615ee
jacksheehy15 <sheehyboi@gmail.com>
2021-04-30 14:55:20 +0000

    Add update category functionality in Db

--- app.py
+++ app.py
@@ -187,6 +187,13 @@ def add_category():
 
 @app.route("/edit_category/<category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("You have updated a category!")
+        return redirect(url_for("get_categories"))
     category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
     return render_template("edit_category.html", category=category)
 
